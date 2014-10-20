9f0c9edc92376e28083dd1f88a82dda85d46a447
Maggi Trymbill <maggi@trymbill.is>
2014-10-20 00:52:51 -0700

    Fixed a few issues with fetching config variables from plist

--- src/ios/AppDelegate+ParsePush.m
+++ src/ios/AppDelegate+ParsePush.m
@@ -12,76 +12,137 @@
 }
 
 - (BOOL)application:(UIApplication*)application customDidFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
-  // Get app name
-  NSBundle *bundle = [NSBundle mainBundle];
-  NSDictionary *info = [bundle infoDictionary];
-  NSString *prodName = [info objectForKey:@"CFBundleDisplayName"];
+    // Get app info
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSDictionary *info = [bundle infoDictionary];
 
-  // Fetch parse plist file and get app id and client key from it
-  NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:[NSString stringWithFormat:@"%@%@", prodName, @"-Info"]; ofType:@"plist"]];
-  NSString *app_id = [dictionary objectForKey:@"ParseAppID"];
-  NSString *client_key = [dictionary objectForKey:@"ParseClientKey"];
+    NSString *app_id = [[info objectForKey:@"ParseAppID"] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
+    NSString *client_key = [[info objectForKey:@"ParseClientKey"] stringByReplacingOccurrencesOfString:@"\"" withString:@""];
 
-  [Parse setApplicationId:app_id clientKey:client_key];
+    [Parse setApplicationId:app_id clientKey:client_key];
 
-  [[PFInstallation currentInstallation] setObject:[PFUser currentUser] forKey:@"user"];
-  [[PFInstallation currentInstallation] saveEventually];
-
-  // Register for Push Notitications, if running iOS 8
-  if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
-    UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
-                                                    UIUserNotificationTypeBadge |
-                                                    UIUserNotificationTypeSound);
-    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
-                                                                             categories:nil];
-    [application registerUserNotificationSettings:settings];
-    [application registerForRemoteNotifications];
-  } else {
-    // Register for Push Notifications before iOS 8
-    [application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
-                                                     UIRemoteNotificationTypeAlert |
-                                                     UIRemoteNotificationTypeSound)];
-  }
-
-  if (launchOptions != nil) {
-    NSDictionary* notification = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
-    if (notification != nil) {
-      NSLog(@"Opened: %@", notification);
+    [PFUser enableAutomaticUser];
+    
+    PFACL *defaultACL = [PFACL ACL];
+    [defaultACL setPublicReadAccess:YES];
+    
+    [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
+    
+    if (application.applicationState != UIApplicationStateBackground) {
+        BOOL preBackgroundPush = ![application respondsToSelector:@selector(backgroundRefreshStatus)];
+        BOOL oldPushHandlerOnly = ![self respondsToSelector:@selector(application:didReceiveRemoteNotification:fetchCompletionHandler:)];
+        BOOL noPushPayload = ![launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+        if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
+            [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+            
+            NSDictionary* notification = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+            if (notification != nil) {
+                NSLog(@"Opened: %@", notification);
+            }
+        }
     }
-  }
+    
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
+    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                        UIUserNotificationTypeBadge |
+                                                        UIUserNotificationTypeSound);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                                 categories:nil];
+        [application registerUserNotificationSettings:settings];
+        [application registerForRemoteNotifications];
+    } else
+#endif
+    {
+        [application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
+                                                         UIRemoteNotificationTypeAlert |
+                                                         UIRemoteNotificationTypeSound)];
+    }
 
-  return [self application:application customDidFinishLaunchingWithOptions:launchOptions];
+    return [self application:application customDidFinishLaunchingWithOptions:launchOptions];
 }
 
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-  // Store the deviceToken in the current installation and save it to Parse.
-  PFInstallation *currentInstallation = [PFInstallation currentInstallation];
-  [currentInstallation setDeviceTokenFromData:deviceToken];
-  [currentInstallation saveInBackground];
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken {
+    [PFPush storeDeviceToken:newDeviceToken];
+    [PFPush subscribeToChannelInBackground:@"" target:self selector:@selector(subscribeFinished:error:)];
 }
+
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-  if (error.code == 3010) {
-    NSLog(@"Push notifications are not supported in the iOS Simulator.");
-  } else {
-    // show some alert or otherwise handle the failure to register.
-    NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
-  }
+    if (error.code == 3010) {
+        NSLog(@"Push notifications are not supported in the iOS Simulator.");
+    } else {
+        // show some alert or otherwise handle the failure to register.
+        NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
+    }
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-  if([application applicationState] == UIApplicationStateInactive) {
-    NSLog(@"Received notification while inactive: %@", userInfo);
     [PFPush handlePush:userInfo];
-  }
-  else
-  {
-    NSLog(@"Received notifications while active: %@", userInfo);
-  }
+    
+    if (application.applicationState == UIApplicationStateInactive) {
+        [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
+    }
+}
+
+///////////////////////////////////////////////////////////
+// Uncomment this method if you want to use Push Notifications with Background App Refresh
+///////////////////////////////////////////////////////////
+/*
+ - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+ if (application.applicationState == UIApplicationStateInactive) {
+ [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
+ }
+ }
+ */
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state.
+     This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message)
+     or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates.
+     Games should use this method to pause the game.
+     */
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state
+     information to restore your application to its current state in case it is terminated later.
+     If your application supports background execution,
+     this method is called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+     */
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-  NSLog(@"Active - Set Badge Number to Zero");
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     Save data if appropriate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+#pragma mark - ()
+
+- (void)subscribeFinished:(NSNumber *)result error:(NSError *)error {
+    if ([result boolValue]) {
+        NSLog(@"ParsePush successfully subscribed to push notifications on the broadcast channel.");
+    } else {
+        NSLog(@"ParsePush failed to subscribe to push notifications on the broadcast channel.");
+    }
 }
 
 @end
