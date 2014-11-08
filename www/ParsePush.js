c04fabf0d6efc6695aa20cb9d0cf1db24f3d2004
Maggi Trymbill <maggi@trymbill.is>
2014-11-07 19:43:11 -0800

    Fixed inconsistency in code

--- www/ParsePush.js
+++ www/ParsePush.js
@@ -1,71 +1,46 @@
 
-var CC;
-(function (CC) {
-    var CordovaParse = (function () {
-        function CordovaParse() {
+var exec = require('cordova/exec');
+
+var ParsePush = {
+    sendNotification: function (data, successCallback, failCallback) {
+        exec(successCallback, failCallback, 'ParsePush', 'sendNotification', [data]);
+    },
+    getNotification: function (data, successCallback, failCallback) {
+        exec(successCallback, failCallback, 'ParsePush', 'getNotification', [data]);
+    },
+    receiveNotification: function (data) {
+        console.log(data);
+    },
+
+    getInstallationId: function (successcb, failcb) {
+        if (!window.cordova) {
+            if (failcb)
+                failcb("no cordova");
+            return;
         }
+        exec(function (response) {
+            successcb(response);
+        }, function (err) {
+            console.log("getAppVersion call failed with error: " + err);
+            if (failcb)
+                failcb(err);
+        }, "ParsePush", "getInstallationId", []);
+    },
 
-        CordovaParse.prototype.getInstallationId = function (successcb, failcb) {
-            if (!window.cordova) {
-                if (failcb)
-                    failcb("no cordova");
-                return;
-            }
-            window.cordova.exec(function (response) {
-                successcb(response);
-            }, function (err) {
-                console.log("getAppVersion call failed with error: " + err);
-                if (failcb)
-                    failcb(err);
-            }, "CordovaParse", "getInstallationId", []);
-        };
+    setKeyValue: function (key, value, successcb, failcb) {
+        if (!window.cordova) {
+            if (failcb)
+                failcb("no cordova");
+            return;
+        }
+        exec(function (response) {
+            successcb();
+        }, function (err) {
+            console.log("setKeyValue call failed with error: " + err);
+            if (failcb)
+                failcb(err);
+        }, "ParsePush", "setKeyValue", [key, value]);
+    }
+}
 
-        CordovaParse.prototype.setKeyValue = function (key, value, successcb, failcb) {
-            if (!window.cordova) {
-                if (failcb)
-                    failcb("no cordova");
-                return;
-            }
-            window.cordova.exec(function (response) {
-                successcb();
-            }, function (err) {
-                console.log("setKeyValue call failed with error: " + err);
-                if (failcb)
-                    failcb(err);
-            }, "CordovaParse", "setKeyValue", [key, value]);
-        };
-
-        CordovaParse.prototype.sendNotification = function (data, successCallback, failCallback) {
-            if (!window.cordova) {
-                if (failcb)
-                    failcb("no cordova");
-                return;
-            }
-            window.cordova.exec(successCallback, failCallback, 'ParsePush', 'sendNotification', [data]);
-        };
-
-        CordovaParse.prototype.getNotification = function (data, successCallback, failCallback) {
-            if (!window.cordova) {
-                if (failcb)
-                    failcb("no cordova");
-                return;
-            }
-            window.cordova.exec(successCallback, failCallback, 'ParsePush', 'getNotification', [data]);
-        };
-
-        CordovaParse.prototype.receiveNotification = function (data) {
-            if (!window.cordova) {
-                if (failcb)
-                    failcb("no cordova");
-                return;
-            }
-            console.log(data);
-        };
-
-        return CordovaParse;
-    })();
-    
-    CC.CordovaParse = CordovaParse;
-})(CC || (CC = {}));
-
-module.exports = CC;
+module.exports = ParsePush;
