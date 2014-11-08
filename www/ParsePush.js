db2a8e86647b0628cf3750a85e753b519d971ab5
Maggi Trymbill <maggi@trymbill.is>
2014-11-07 18:19:18 -0800

    fmt

--- www/ParsePush.js
+++ www/ParsePush.js
@@ -1,15 +1,15 @@
 var exec = require('cordova/exec');
 
 var ParsePush = {
-  sendNotification: function(data, successCallback, failCallback) {
-    exec(successCallback, failCallback, 'ParsePush', 'sendNotification', [data]);
-  },
-  getNotification: function(data, successCallback, failCallback) {
-    exec(successCallback, failCallback, 'ParsePush', 'getNotification', [data]);
-  },
-  receiveNotification: function(data) {
-    console.log(data);
-  }
+    sendNotification: function(data, successCallback, failCallback) {
+        exec(successCallback, failCallback, 'ParsePush', 'sendNotification', [data]);
+    },
+    getNotification: function(data, successCallback, failCallback) {
+        exec(successCallback, failCallback, 'ParsePush', 'getNotification', [data]);
+    },
+    receiveNotification: function(data) {
+        console.log(data);
+    }
 }
 
 module.exports = ParsePush;
